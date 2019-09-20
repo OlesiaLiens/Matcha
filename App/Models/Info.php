8799ab969ea0b2841bb96b796f5012024b0c1aca
Andrii Makarenko <amakaren@student.unit.ua>
2019-09-20 18:33:52 +0300

    Removed single tag reference

--- App/Models/Info.php
+++ App/Models/Info.php
@@ -46,7 +46,7 @@ class Info extends \Core\Model
 	{
 		$db = static::getDB();
 
-		if ($this->date && $this->gender && $this->preferences  && ctype_alpha($this->city) && $this->tags && $this->bio) {
+		if ($this->date && $this->gender && $this->preferences  && ctype_alpha($this->city) && $this->bio) {
 			$save = $db->prepare("UPDATE USERS SET bday = ?, gender = ?, preference = ?, bio = ?, location = ? WHERE  id = ?");
 			$save->execute([$this->date, $this->gender, $this->preferences, $this->bio, $this->city, $user]);
 			$tag_id = $db->prepare("SELECT id FROM tags WHERE tag = ?");
