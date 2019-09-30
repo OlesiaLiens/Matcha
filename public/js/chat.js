2cbc0ddfd845188f88981a8f12715afd1e86a89a
Andrii Makarenko <amakaren@student.unit.ua>
2019-09-30 14:01:05 +0300

    Integrate new chat to the main files

--- public/js/chat.js
+++ public/js/chat.js
@@ -12,28 +12,162 @@
 //      "*%%%%%%**~  ""    "**"`    //
 //——————————————————————————————————//
 
+let useravatar = '';
+let dialogues;
+let counterpart;
+let msgCounter;
+
+const getAvatar = () => {
+	$.ajax({
+		url: './getAvatar.php',
+		type: 'get',
+		success: response => {useravatar = response}
+	});
+}
+
 const loadMessages = () => {
 	$.ajax({
-		url: '/chat/getdialogues/',
+		url: './getJSON.php',
 		type: 'get',
 		success: dialoguesJSON => {
-			let dialogues = JSON.parse(dialoguesJSON);
-			Object.keys(dialogues).forEach(dialogue => {
-				console.log(dialogues[dialogue]);
+			dialogues = JSON.parse(dialoguesJSON);
+			Object.keys(dialogues).forEach(key => {
+				let dialogue = dialogues[key];
+
+				let contacts = document.getElementById('contacts');
 				let dialogueLi = document.createElement('li');
+				dialogueLi.setAttribute('class', 'dialogue');
+				dialogueLi.setAttribute('name', key);
+				dialogueLi.onclick = openDialogue;
 				let dialogueDiv = document.createElement('div');
+				dialogueDiv.setAttribute('class', 'd-flex bd-highlight');
 				let avatarDiv = document.createElement('div');
+				avatarDiv.setAttribute('class', 'img_cont');
+				let avatar = document.createElement('img');
+				avatar.setAttribute('class', 'rounded-circle user_img');
+				avatar.src = dialogue.avatar;
+				let onlineIcon = document.createElement('span');
+				onlineIcon.setAttribute('class', getOnlineClass(dialogue));
 				let infoDiv = document.createElement('div');
-				
+				infoDiv.setAttribute('class', 'user_info');
+				let nameDiv = document.createElement('span');
+				nameDiv.innerText = `${dialogue.firstName} ${dialogue.lastName}`;
+				let onlineText = document.createElement('p');
+				onlineText.innerText = getOnlineText(dialogue);
+
+				avatarDiv.appendChild(avatar);
+				avatarDiv.appendChild(onlineIcon);
+				infoDiv.appendChild(nameDiv);
+				infoDiv.appendChild(onlineText);
+				dialogueDiv.appendChild(avatarDiv);
+				dialogueDiv.appendChild(infoDiv);
+				dialogueLi.appendChild(dialogueDiv);
+				contacts.appendChild(dialogueLi);
 			});
 		}
 	});
 };
 
+const openDialogue = event => {
+	let target;
+	if (event.target.classList.contains('dialogue'))
+		target = event.target;
+	else
+		target = event.target.closest('.dialogue');
+	let selectedDialogue = target.getAttribute('name');
+	counterpart = dialogues[selectedDialogue];
+
+	Array.from(document.getElementsByClassName('dialogue')).forEach(
+		dialogue => {dialogue.classList.remove('active')});
+	target.classList.add('active');
+
+	$('#selectedAvatar').attr('src', counterpart.avatar);
+	$('#title').text(`Chat with ${counterpart.firstName} ${counterpart.lastName}`);
+	$('#selectedOnline').attr('class', getOnlineClass(counterpart));
+	msgCounter = Object.keys(counterpart.messages).length;
+	$('#counter').text(`${msgCounter} messages`);
+	$('#chatHeader').show();
+
+	$('#messagesBlock').empty();
+	Array.from(counterpart.messages).forEach(drawMessage);
+}
+
+const drawMessage = message => {
+	console.log(message);
+	let messageBox = document.createElement('div');
+	let just = (message.sender == 'they') ? 'start' : 'end';
+	messageBox.setAttribute('class', `d-flex justify-content-${just} mb-4`);
+
+	let avatarDiv = document.createElement('div');
+	avatarDiv.setAttribute('class', 'img_cont_msg');
+	let avatar = document.createElement('img');
+	avatar.setAttribute('class', 'rounded-circle user_img_msg');
+	let userpic = message.sender == 'they' ? counterpart.avatar : useravatar;
+	avatar.setAttribute('src', userpic);
+
+	let messageDiv = document.createElement('div');
+	let msgClassExt = (message.sender == 'they') ? '' : '_send';
+	messageDiv.setAttribute('class', `msg_cotainer${msgClassExt}`);
+	messageDiv.innerText = message.text;
+	let messageTime = document.createElement('span');
+	messageTime.setAttribute('class', `msg_time${msgClassExt}`);
+	messageTime.innerText = message.time;
+
+	if (message.sender == 'they') {
+		$('#messagesBlock').append(
+			$(messageBox)
+			.append($(avatarDiv).append(avatar))
+			.append($(messageDiv).append(messageTime))
+		);	
+	} else {
+		$('#messagesBlock').append(
+			$(messageBox)
+			.append($(messageDiv).append(messageTime))
+			.append($(avatarDiv).append(avatar))
+		);
+	}
+}
+
+const getOnlineClass = dialogue => {
+	if (dialogue.lastOnline == 'now')
+		return 'online_icon'
+	else
+		return 'online_icon offline'
+}
+
+const getOnlineText = dialogue => {
+	if (dialogue.lastOnline == 'now')
+		return `${dialogue.firstName} is online`
+	else
+		return `${dialogue.firstName} was online ${dialogue.lastOnline}`
+}
+
+/* This finction is needed because without it, the hours or minutes 
+   below 10 would be single-digit, e.g. 16:2, not 16:02 */
+const getTimeString = () => {
+	var date = new Date();
+	currentHours = date.getHours();
+	currentHours = ("0" + currentHours).slice(-2);
+	currentMinutes = date.getMinutes();
+	currentMinutes = ("0" + currentMinutes).slice(-2);
+	return `${currentHours}:${currentMinutes}, Today`;
+}
+
 $(document).ready(() => {
-	$('#action_menu_btn').click(() =>{
+	$('#action_menu_btn').click(() => {
 		$('.action_menu').toggle();
 	});
+	$('.send_btn').click(() => {
+		let message = {
+			'sender' : 'you',
+			'text' : $('#msgInput').val(),
+			'time' : getTimeString()
+		};
+		drawMessage(message);
+		$('#counter').text(`${msgCounter += 1} messages`);
+		$('#msgInput').val('');
+	});
+	getAvatar();
 	loadMessages();
 	console.log('ok');
 });
