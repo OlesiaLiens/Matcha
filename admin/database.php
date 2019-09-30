63fc31a463ab134b52625b22f9a3c07c3d0943b6
Andrii Makarenko <amakaren@student.unit.ua>
2019-09-30 15:43:15 +0300

    Proper database creation

--- admin/database.php
+++ admin/database.php
@@ -4,7 +4,7 @@ $DB_DSN = 'mysql:host=localhost:3306';
 $DB_NAME = 'matcha';
 $DB_SHORTCUT = $DB_DSN . ';dbname=' . $DB_NAME;
 $DB_USER = 'root';
-$DB_PASS = 'root';
+$DB_PASS = 'fV71c8Fs';
 
 $options = [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
--- admin/setup.php
+++ admin/setup.php
@@ -3,86 +3,87 @@
 
 include 'database.php';
 
-print('U/N: ' . $DB_USER . PHP_EOL . 'P/W: ' . $DB_PASS . PHP_EOL);
+//print('U/N: ' . $DB_USER . PHP_EOL . 'P/W: ' . $DB_PASS . PHP_EOL);
 $connection = new PDO($DB_DSN, $DB_USER, $DB_PASS, $options);
 
 // Create an empty database
 $connection->exec("DROP DATABASE IF EXISTS " . $DB_NAME);
 $connection->exec("CREATE DATABASE " . $DB_NAME);
-print('DB created' . PHP_EOL);
+//print('DB created' . PHP_EOL);
 $connection->exec("use " . $DB_NAME);
-print('Connected to DB' . PHP_EOL);
+//print('Connected to DB' . PHP_EOL);
 
 $sqlUsers = "CREATE TABLE users(
 				id           INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 				username     varchar(50)  NOT NULL,
-				first_name     varchar(50)  NOT NULL,
+				first_name   varchar(50)  NOT NULL,
 				last_name    varchar(50)  NOT NULL,
-				bday        INTEGER DEFAULT 0     NOT NULL,
+				bday         INTEGER      NOT NULL DEFAULT 0,
 				email        varchar(255) NOT NULL,
 				password     varchar(255) NOT NULL,
 				active       varchar(50)  NOT NULL,
-				gender      VARCHAR(50)   DEFAULT '-' NOT NULL,
-				preference  VARCHAR(50)   DEFAULT '-' NOT NULL,
-				bio         TEXT,
-				longitude   FLOAT,
+				gender       VARCHAR(50)  NOT NULL DEFAULT '-',
+				preference   VARCHAR(50)  NOT NULL DEFAULT 'all',
+				bio          TEXT,
+				longitude    FLOAT,
 				latitude     FLOAT,
 				location     VARCHAR(50)  NOT NULL DEFAULT '-',
 				avatars      varchar(50)  NOT NULL DEFAULT '../images/1photo.png',
 				notification INT          NOT NULL DEFAULT 1,
-				rating       INTEGER      DEFAULT 0 NOT NULL,
-				online      TINYINT(1)          DEFAULT 0 NOT NULL,
-				token        varchar(255)  DEFAULT '' NOT NULL
+				rating       INTEGER      NOT NULL DEFAULT 0,
+				online       TINYINT(1)   NOT NULL DEFAULT 0,
+				last_see     TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+				token        varchar(255) NOT NULL DEFAULT ''
 			)";
 
 $sqlTokens = "CREATE TABLE tokens(
-				id         INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-				user_id    INT UNSIGNED,
-				emailtoken varchar(255) NOT NULL
+				id           INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+				user_id      INT UNSIGNED,
+				emailtoken   VARCHAR(255) NOT NULL
 			)";
 
 $sqlPhotos = "CREATE TABLE photos(
-				id      INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-				path    VARCHAR(255)                        NOT NULL,
-				user_id INT(11) UNSIGNED                    NOT NULL,
-				date    TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+				id           INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+				path         VARCHAR(255) NOT NULL,
+				user_id      INT UNSIGNED NOT NULL,
+				date         TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP
 			)";
 
 $sqlTags = "CREATE TABLE tags(
-				id  INTEGER PRIMARY KEY AUTO_INCREMENT,
-				tag VARCHAR(255) NOT NULL UNIQUE
+				id           INT PRIMARY KEY AUTO_INCREMENT,
+				tag          VARCHAR(255) NOT NULL UNIQUE
 			)";
 
 $sqlUserTags = "CREATE TABLE users_tags(
-				id  INTEGER PRIMARY KEY AUTO_INCREMENT,
-				user_id INTEGER,
-				tag_id  INTEGER, 
-				date    TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+				id           INT PRIMARY KEY AUTO_INCREMENT,
+				user_id      INTEGER,
+				tag_id       INTEGER, 
+				date         TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP 
 			)";
 
 $sqlUserAction = "CREATE TABLE user_action(
 				id           INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-				first_user     varchar(50)   DEFAULT '-' NOT NULL,
-				second_user     varchar(50)  DEFAULT '-'  NOT NULL,
-				see    varchar(50)  DEFAULT '-'  NOT NULL,
-				liked        varchar(50)   DEFAULT '-' NOT NULL,
-				matched        varchar(255)  DEFAULT '-' NOT NULL,
-				ban     varchar(255)  DEFAULT '-' NOT NULL,
-				break       varchar(50)  DEFAULT '-'  NOT NULL,
-				fake       varchar(50)   DEFAULT '-' NOT NULL
+				first_user   VARCHAR(50)  NOT NULL DEFAULT 'none',
+				second_user  VARCHAR(50)  NOT NULL DEFAULT 'none',
+				see          VARCHAR(50)  NOT NULL DEFAULT 'none',
+				liked        VARCHAR(50)  NOT NULL DEFAULT 'none',
+				matched      VARCHAR(255) NOT NULL DEFAULT 'none',
+				ban          VARCHAR(255) NOT NULL DEFAULT 'none',
+				break        VARCHAR(50)  NOT NULL DEFAULT 'none',
+				fake         VARCHAR(50)  NOT NULL DEFAULT 'none'
 			)";
 
 $connection->exec($sqlUsers);
-print('Table Users created' . PHP_EOL);
+//print('Table Users created' . PHP_EOL);
 $connection->exec($sqlTokens);
-print('Table Tokens created' . PHP_EOL);
+//print('Table Tokens created' . PHP_EOL);
 $connection->exec($sqlPhotos);
-print('Table Photos created' . PHP_EOL);
+//print('Table Photos created' . PHP_EOL);
 $connection->exec($sqlTags);
-print('Table Tags created' . PHP_EOL);
+//print('Table Tags created' . PHP_EOL);
 $connection->exec($sqlUserTags);
-print('Table UserTags created' . PHP_EOL);
+//print('Table UserTags created' . PHP_EOL);
 $connection->exec($sqlUserAction);
-print('Table UserAction created' . PHP_EOL);
+//print('Table UserAction created' . PHP_EOL);
 
 ?>
