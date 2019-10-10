5167eb4104036319026b68957126ebd7db4e0a64
Andrii Makarenko <amakaren@student.unit.ua>
2019-10-10 14:45:20 +0300

    Browse elements generate dynamically

--- public/js/browse.js
+++ public/js/browse.js
@@ -16,6 +16,7 @@ let resultsContainer = document.getElementById('resultsContainer');
 let ownLongitude;
 let ownLatitude;
 let ownTags = [];
+let cards = [];
 
 /* I know this creates hella load and makes transaction time
    super long, but the deadlines are too close */
@@ -24,8 +25,64 @@ const getUserGallery = () => {
 		url: '/browse/getusers',
 		type: 'get',
 		success: response => {
-			console.log(JSON.parse(response));
-			
+			responseObj = JSON.parse(response);
+			Object.values(responseObj).forEach(profile =>{
+				console.log(profile);
+				let card = document.createElement('div');
+				card.setAttribute('class', 'card mb-4');
+
+				let avatar = document.createElement('img');
+				avatar.setAttribute('class', 'card-img-top');
+				avatar.setAttribute('src', profile.avatar);
+				card.appendChild(avatar);
+
+				let cb = document.createElement('div');
+				cb.setAttribute('class', 'card-body');
+				card.appendChild(cb);
+
+				let name = document.createElement('h5');
+				name.setAttribute('class', 'card-title');
+				name.innerText = `${profile.firstName} ${profile.lastName}`;
+				cb.appendChild(name);
+
+				let bio = document.createElement('p');
+				bio.setAttribute('class', 'card-text');
+				bio.innerText = profile.bio;
+				cb.appendChild(bio);
+
+				let details = document.createElement('ul');
+				details.setAttribute('class', 'list-group list-group-flush');
+				card.appendChild(details);
+
+				let age = document.createElement('li');
+				age.setAttribute('class', 'list-group-item');
+				age.innerText = `Age: ${profile.age}`;
+				details.appendChild(age);
+
+				let rating = document.createElement('li');
+				rating.setAttribute('class', 'list-group-item');
+				rating.innerText = `Rating: ${profile.rating}`;
+				details.appendChild(rating);
+
+				let location = document.createElement('li');
+				location.setAttribute('class', 'list-group-item');
+				location.innerText = `Age: ${profile.location}`;
+				details.appendChild(location);
+
+				let footer = document.createElement('div');
+				footer.setAttribute('class', 'card-footer text-center');
+				card.appendChild(footer);
+
+				let viewProfBtn = document.createElement('button');
+				viewProfBtn.setAttribute('class', 'btn btn-primary');
+				viewProfBtn.innerText = 'View profile';
+				footer.appendChild(viewProfBtn);
+
+				card.profile = profile;
+				cards.push(card);
+				resultsContainer.appendChild(card);
+			});
+			console.log(cards);
 		}
 	})
 }
